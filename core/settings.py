e59d3b68a555ee30ddbd7f759e30af17831aad28
Yukhosmith <yuskhosmith1@gmail.com>
2023-04-07 13:55:50 +0100

    full-duplex connection established sucessfully

--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# ASGI_APPLICATION = "core.routing.application"
+ASGI_APPLICATION = "core.routing.application"
 # for local, redis is the go to for channel layer
 CHANNEL_LAYERS = {
     "default": {
